462dd17126763e797a5c8131447ff630742c718a
javed-iqubal <jiqbal07@gmail.com>
2026-04-09 12:18:10 +0530

    struct - return pointer, using factory, constructor example

--- 04struct/composition/Address.go
+++ 04struct/composition/Address.go
@@ -10,7 +10,7 @@ type Address struct {
 type Person struct {
 	FirstName string
 	LastName  string
-	Address   Address
+	Address
 }
 
 func PrintDetails() {
@@ -25,5 +25,71 @@ func PrintDetails() {
 		},
 	}
 	fmt.Println(person)
-	fmt.Printf("Name: %s %s \nAddress: %s %s \n", person.FirstName, person.LastName, person.Address.City, person.Address.State)
+	fmt.Printf("Name: %s %s \nAddress: %s %s \n\n", person.FirstName, person.LastName, person.Address.City, person.Address.State)
+}
+
+func PrintDetailsUsingPointerToStruct() {
+
+	person := &Person{
+
+		FirstName: "Liza",
+		LastName:  "Fatma",
+		Address: Address{
+			City:  "Hyderabad",
+			State: "Telangana",
+		},
+	}
+	fmt.Println(&person)
+	fmt.Println(person)
+	fmt.Printf("Name: %s %s \nAddress: %s %s \n\n", person.FirstName, person.LastName, person.Address.City, person.Address.State)
+	//
+	fmt.Println(*person)
+	fmt.Printf("Name: %s %s \nAddress: %s %s \n\n", (*person).FirstName, (*person).LastName, (*person).Address.City, (*person).Address.State)
+
+}
+
+func CreatePerson(FirstName string, LastName string, Address Address) Person {
+	person := Person{
+		FirstName: FirstName,
+		LastName:  LastName,
+		Address:   Address,
+	}
+	return person
+}
+
+func CreatePersonRetunPointer(FirstName string, LastName string, Address Address) *Person {
+	person := Person{
+		FirstName: FirstName,
+		LastName:  LastName,
+		Address:   Address,
+	}
+	return &person
+}
+
+/*
+*
+
+	Factory function to initialize the structs with parameter
+	Constructors: newFunctionName(args) returnType -> Since Go does not support constructor like other languages, constructors are specified via factory functions
+*/
+func newPerson(FirstName string, LastName string, Address Address) *Person {
+	person := Person{
+		FirstName: FirstName,
+		LastName:  LastName,
+		Address:   Address,
+	}
+	return &person
+}
+
+func PersonFactory() *Person {
+
+	person := newPerson(
+		"Maryam",
+		"Fatma",
+		Address{
+			City:  "Noida",
+			State: "UP",
+		},
+	)
+	return person
 }
--- 04struct/main.go
+++ 04struct/main.go
@@ -1,11 +1,40 @@
 package main
 
 import (
+	"fmt"
 	"struct/composition"
 )
 
 func main() {
 
-	composition.PrintDetails()
+	// composition.PrintDetails()
+	// composition.PrintDetailsUsingPointerToStruct()
+
+	person := composition.CreatePerson(
+		"Maryam",
+		"Fatma",
+		composition.Address{
+			City:  "Noida",
+			State: "UP",
+		},
+	)
+	fmt.Printf("Name: %s %s \n", person.FirstName, person.LastName)
+	fmt.Printf("Address: %s %s \n\n", person.City, person.State)
+
+	person2 := composition.CreatePersonRetunPointer(
+		"Maryam",
+		"Fatma",
+		composition.Address{
+			City:  "Noida",
+			State: "UP",
+		},
+	)
+	fmt.Printf("Name: %s %s \n", person2.FirstName, person2.LastName)
+	fmt.Printf("Address: %s %s \n\n", person2.City, person2.State)
+
+	person3 := composition.PersonFactory()
+
+	fmt.Printf("Name: %s %s \n", person3.FirstName, person3.LastName)
+	fmt.Printf("Address: %s %s \n\n", person3.City, person3.State)
 
 }
